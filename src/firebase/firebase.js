82a83324e1c35ff311c3b5a877b822306de13f61
Charlie Knoll <charlie.knoll@gmail.com>
2024-12-09 11:49:03 -0700

    Adds support for moneyballs using charlie's firebase config and node v22

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,34 +1,28 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getAuth, initializeAuth, indexedDBLocalPersistence } from 'firebase/auth'
-import { Capacitor } from '@capacitor/core'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  initializeAuth,
+  indexedDBLocalPersistence,
+} from "firebase/auth";
+import { Capacitor } from "@capacitor/core";
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDfH3zGn8105ao6uXAy-hQzcXSDmg4ooVc',
-  authDomain: 'moneyballs---firestore-6d986.firebaseapp.com',
-  projectId: 'moneyballs---firestore-6d986',
-  storageBucket: 'moneyballs---firestore-6d986.appspot.com',
-  messagingSenderId: '480300453984',
-  appId: '1:480300453984:web:b7553ca734fafe704b7305'
-}
+import { firebaseConfig } from "./firebaseConfig";
 
 function whichAuth() {
-  let auth
+  let auth;
   if (Capacitor.isNativePlatform()) {
     auth = initializeAuth(app, {
-      persistence: indexedDBLocalPersistence
-    })
+      persistence: indexedDBLocalPersistence,
+    });
   } else {
-    auth = getAuth()
+    auth = getAuth();
   }
-  return auth
+  return auth;
 }
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
-const auth = whichAuth()
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = whichAuth();
 
-export {
-  db,
-  auth
-}
+export { db, auth };
--- src/stores/storeAuth.js
+++ src/stores/storeAuth.js
@@ -1,107 +1,111 @@
-import { defineStore } from 'pinia'
-import { reactive, ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { Dialog } from 'quasar'
-import { createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
-import { auth } from 'src/firebase/firebase'
-import { useStoreEntries } from 'src/stores/storeEntries'
+import { defineStore } from "pinia";
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import { Dialog } from "quasar";
+import {
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "src/firebase/firebase";
+import { useStoreEntries } from "src/stores/storeEntries";
 
-export const useStoreAuth = defineStore('auth', () => {
-
+export const useStoreAuth = defineStore("auth", () => {
   /*
     state
   */
 
-    const authInitialized = ref(false)
+  const authInitialized = ref(false);
 
-    const userDetailsDefault = {
-      id: null,
-      email: null
-    }
+  const userDetailsDefault = {
+    id: null,
+    email: null,
+  };
 
-    const userDetails = reactive({
-      ...userDetailsDefault
-    })
-
+  const userDetails = reactive({
+    ...userDetailsDefault,
+  });
 
   /*
     actions
   */
-  
-    const init = () => {
-      const router = useRouter(),
-            storeEntries = useStoreEntries()
 
-      onAuthStateChanged(auth, (user) => {
-        authInitialized.value = true
-        if (user) {
-          userDetails.id = user.uid
-          userDetails.email = user.email
-          router.push('/')
-          storeEntries.init()
-        } else {
-          Object.assign(userDetails, userDetailsDefault)
-          router.replace('/auth')
-          storeEntries.clearAndStopEntries()
-        }
-      })
-    }
+  const init = () => {
+    const router = useRouter(),
+      storeEntries = useStoreEntries();
 
-    const registerUser = ({ email, password }) => {
-      createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        userDetails.id = user.uid;
+        userDetails.email = user.email;
+        if (authInitialized.value) router.push("/");
+        storeEntries.init();
+      } else {
+        Object.assign(userDetails, userDetailsDefault);
+        router.replace("/auth");
+        storeEntries.clearAndStopEntries();
+      }
+      authInitialized.value = true;
+    });
+  };
+
+  const registerUser = ({ email, password }) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
         // const user = userCredential.user
         // console.log('user: ', user)
-      }).catch((error) => {
-        showFirebaseError(error.message)
       })
-    }
+      .catch((error) => {
+        showFirebaseError(error.message);
+      });
+  };
 
-    const loginUser = ({ email, password }) => {
-      signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+  const loginUser = ({ email, password }) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
         // const user = userCredential.user
         // console.log('user: ', user)
-      }).catch((error) => {
-        showFirebaseError(error.message)
       })
-    }
+      .catch((error) => {
+        showFirebaseError(error.message);
+      });
+  };
 
-    const logoutUser = () => {
-      signOut(auth).then(() => {
+  const logoutUser = () => {
+    signOut(auth)
+      .then(() => {
         // console.log('user was logged out')
-      }).catch((error) => {
-        showFirebaseError(error.message)
       })
-    }
-
+      .catch((error) => {
+        showFirebaseError(error.message);
+      });
+  };
 
   /*
     helpers
   */
-  
-    const showFirebaseError = message => {
-      Dialog.create({
-        title: 'Error',
-        message
-      })
-    }
 
+  const showFirebaseError = (message) => {
+    Dialog.create({
+      title: "Error",
+      message,
+    });
+  };
 
   /*
     return
   */
-  
-    return { 
 
-      // state
-      authInitialized,
-      userDetails,
+  return {
+    // state
+    authInitialized,
+    userDetails,
 
-      // actions
-      init,
-      registerUser,
-      loginUser,
-      logoutUser
-
-    }
-    
-})
+    // actions
+    init,
+    registerUser,
+    loginUser,
+    logoutUser,
+  };
+});
